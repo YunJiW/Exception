62a33ef6522e28b1f0cbdb2ac6fccb40719cbe51
YunJiW <dtroy371@gmail.com>
2024-03-12 15:01:48 +0900

    chore : javax 로 적은 거 수정

--- src/main/java/hello/exception/servlet/ErrorPageController.java
+++ src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -13,7 +13,7 @@ public class ErrorPageController {
     //RequestDispatcher 상수로 정의되어 있음
     public static final String ERROR_EXCEPTION = "jakarta.servlet.error.exception";
     public static final String ERROR_EXCEPTION_TYPE = "jakarta.servlet.error.exception_type";
-    public static final String ERROR_MESSAGE = "javax.servlet.error.message";
+    public static final String ERROR_MESSAGE = "jakarta.servlet.error.message";
     public static final String ERROR_REQUEST_URI = "jakarta.servlet.error.request_uri";
     public static final String ERROR_SERVLET_NAME = "jakarta.servlet.error.servlet_name";
     public static final String ERROR_STATUS_CODE = "jakarta.servlet.error.status_code";
