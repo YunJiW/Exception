0cc6e6c9395fba0e2c595610cfdb5fd4bb53b0ab
YunJiW <dtroy371@gmail.com>
2024-04-03 14:39:22 +0900

    fix : response 추가

--- src/main/java/hello/exception/resolver/UserHandlerExceptionResolver.java
+++ src/main/java/hello/exception/resolver/UserHandlerExceptionResolver.java
@@ -16,27 +16,34 @@ import java.util.Map;
 public class UserHandlerExceptionResolver implements HandlerExceptionResolver {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
 
-        try{
-            if(ex instanceof UserException){
+        try {
+            if (ex instanceof UserException) {
                 log.info("UserException resolver to 400");
                 String acceptHeader = request.getHeader("accept");
                 response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 
-                if("application/json".equals(acceptHeader)){
-                    Map<String,Object> errorResult = new HashMap<>();
-                    errorResult.put("ex",ex.getClass());
-                    errorResult.put("message",ex.getMessage());
-                    String result = objectMapper.writeValueAsString(errorResult);
-                }else{
+                if ("application/json".equals(acceptHeader)) {
+                    Map<String, Object> errorResult = new HashMap<>();
+                    errorResult.put("ex", ex.getClass());
+                    errorResult.put("message", ex.getMessage());
+                    String result = objectMapper.writeValueAsString(errorResult)
+
+                    response.setContentType("application/json");
+                    response.setCharacterEncoding("utf-8");
+                    response.getWriter().write(result);
+
+                    return new ModelAndView();
+                } else {
                     return new ModelAndView("error/500");
                 }
             }
 
-        }catch (IOException e){
-            log.error("resolver ex",e);
+        } catch (IOException e) {
+            log.error("resolver ex", e);
         }
 
         return null;
